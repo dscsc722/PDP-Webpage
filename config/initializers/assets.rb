740004379172d8054d8cb79c38d00baa09256f04
Xin Wei Liu <xinweili@buffalo.edu>
2017-04-15 14:52:04 -0400

    added images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += ['font-awesome.min.css', 'main.css', 'noscript.css']
+Rails.application.config.assets.precompile += ['font-awesome.min.css', 'FontAwesome.otf','fontawesome-webfont.svg','fontawesome-webfont.woff','overlay.png','main.css', 'noscript.css']
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
